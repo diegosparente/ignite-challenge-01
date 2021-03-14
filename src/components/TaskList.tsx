5320e280ade55667045fb4b38be58748ed6097b7
Diego Parente <diego.s.parente@gmail.com>
2021-03-14 02:24:26 -0300

    handleToggleTaskCompletion concluida

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -30,6 +30,11 @@ export function TaskList() {
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const mapedTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task)
+    setTasks(mapedTasks)
   }
 
   function handleRemoveTask(id: number) {
